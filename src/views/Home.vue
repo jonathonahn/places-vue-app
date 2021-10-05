698e6acd7ae0952381ac57ba13fcadeada19e61c
jonathon <ahnjon77@gmail.com>
2021-10-05 15:11:19 -0700

    adds CRUD index function, loads Places on page load, adds CRUD create function, updates page in realtime

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <div>{{ newPlaceParams }}</div>
+    Name: <input type="text" v-model="newPlaceParams.name" /> Address:
+    <input type="text" v-model="newPlaceParams.address" />
+    <br />
+    <button v-on:click="createPlace">Create Place</button>
+    <div v-for="place in places" v-bind:key="place.id">
+      <p>{{ place }}</p>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style></style>
 
+<script>
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to the Places App!",
+      places: [],
+      newPlaceParams: {},
+    };
+  },
+  created: function () {
+    this.indexPlaces();
+  },
+  methods: {
+    indexPlaces: function () {
+      axios.get("http://localhost:3000/places").then((response) => {
+        console.log(response.data);
+        this.places = response.data;
+      });
+    },
+    createPlace: function () {
+      // foo
+      console.log("test");
+      var params = this.newPlaceParams;
+      axios
+        .post(`http://localhost:3000/places/`, params)
+        .then((response) => {
+          console.log(response.data);
+          this.places.push(response.data);
+          this.newPlaceParams.name = "";
+          this.newPlaceParams.address = "";
+        })
+        .catch((error) => {
+          console.log(error.response.data.errors);
+        });
+    },
   },
 };
 </script>
